be45c1bdf47e5e8fd74262324133145d79f69526
bmadamo98 <36278212+bmadamo98@users.noreply.github.com>
2021-03-18 23:31:35 -0700

    here we go.

--- angular-proj-frontend/src/app/app-routing.module.ts
+++ angular-proj-frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: '**', component: HomeComponent}
 ];
 
